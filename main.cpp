a69236fde60f38a6b685481512f219a7bc34a457
TheHolyBread <leonardoaf2008@icloud.com>
2024-11-21 22:02:01 -0600

    jumpy

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     }
 
     // make window
-    SDL_Window* win = SDL_CreateWindow("RACISM SIMULATOR", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920 / 2, 1080 / 2, 0);
+    SDL_Window* win = SDL_CreateWindow("Bagel Dash", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920 / 2, 1080 / 2, 0);
 
     // make renderer
     SDL_Renderer* rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
@@ -22,34 +22,57 @@ int main(int argc, char *argv[])
     SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, img);
 
     // setup player rectangle
-    int x = 0;
-    int y = 0;
     SDL_Rect plr;
     SDL_QueryTexture(tex, NULL, NULL, &plr.w, &plr.h);
     plr.w /= 10;
     plr.h /= 10;
 
-    plr.x = 0;
-    plr.y = 0;
+    plr.x = 50;
+    plr.y = 1920 / 4;
 
     int speed = 300;
+    int pvel = 0;
 
 
     // game loop
     bool running = true;
+    bool spaced = false;
+    bool spacing = false;
 
     while (running) {
         SDL_Event event;
-        SDL_RenderClear(rend);
-        SDL_RenderCopy(rend, tex, NULL, &plr);
-        SDL_RenderPresent(rend);
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
                 case SDL_QUIT:
                     running = false;
                     break;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.scancode) {
+                        case SDL_SCANCODE_SPACE:
+                            spacing = true;
+                            break;
+                        default:
+                            break;
+                    }
+                case SDL_KEYUP:
+                    case SDL_SCANCODE_SPACE:
+                        spacing = false;
+                        break;
             }
         }
+        if (spacing && plr.y == 1920 / 4) {
+            spaced = true;
+        }
+        if (spaced) {
+            spaced = false;
+            pvel = 10;
+        }
+        pvel--;
+        plr.y -= pvel;
+        plr.y = __min(plr.y, 1920 / 4);
+        SDL_RenderClear(rend);
+        SDL_RenderCopy(rend, tex, NULL, &plr);
+        SDL_RenderPresent(rend);
         SDL_Delay(1000 / 60);
     }
  
